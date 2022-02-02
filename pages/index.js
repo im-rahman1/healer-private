4ec616bf4fd0d1c2e38e00695f18a650a8349cea
im-rahman1 <mjc8808@gmail.com>
2022-02-02 18:44:05 +0500

    done with carousel

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,55 @@
 import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <Carousel
+        className={styles.carousel_imgContainer}
+        showStatus={false}
+        showThumbs={false}
+        showArrows={false}
+        showIndicators
+        useKeyboardArrows
+        infiniteLoop
+        emulateTouch
+        stopOnHover
+        autoPlay>
+        <div className={styles.carousel_imgContainer}>
+          <Image
+            priority
+            src="/homeImg1.jpg"
+            className={styles.carousel_img}
+            alt="homeImg1"
+            layout="responsive"
+            width={350}
+            height={100}
+          />
+        </div>
+        <div className={styles.carousel_imgContainer}>
+          <Image
+            src="/homeImg2.jpg"
+            className={styles.carousel_img}
+            alt="homeImg1"
+            layout="responsive"
+            width={350}
+            height={100}
+          />
+        </div>
+        <div className={styles.carousel_imgContainer}>
+          <Image
+            src="/homeImg3.jpg"
+            className={styles.carousel_img}
+            alt="homeImg1"
+            layout="responsive"
+            width={350}
+            height={100}
+          />
+        </div>
+      </Carousel>
     </Layout>
   );
 }
