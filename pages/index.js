4cfc73eb5182313afa3a40fe3a603cc20299cdd0
im-rahman1 <mjc8808@gmail.com>
2022-02-05 19:31:11 +0500

    din

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,6 @@ export default function Home() {
         }}
         autoplay={{
           delay: 3000,
-          disableOnInteraction: false,
         }}
         loop={true}
         modules={[Pagination, Autoplay]}
