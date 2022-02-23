ab23841078189cce92478a6dcb73544d301b3801
Muhammad Rahman <mjc8808@gmail.com>
2022-02-24 02:55:36 +0500

    added terms and privacyPolicy

--- pages/terms.js
+++ pages/terms.js
@@ -1,9 +1,313 @@
 import Layout from "../components/Layout";
+import { Typography } from "@mui/material";
+import styles from "../styles/terms.module.css";
 
 export default function Terms() {
   return (
     <Layout title="Terms & Conditions">
-      <h1>Terms & Conditions</h1>
+      <div className={styles.termsPage}>
+        <Typography
+          variant="h4"
+          sx={{ marginTop: "20px", marginBottom: "20px" }}>
+          Healercare Pvt Ltd Terms and Conditions
+        </Typography>
+        <Typography variant="body1">
+          The terms and conditions below outline how you may use healer.pk and
+          all associated webpages. It is important you read and understand the
+          terms and conditions carefully before you purchase anything from our
+          site. Please check that you fully understand and agree to all the
+          points outlined before using this site. It is important to acknowledge
+          that these terms and conditions may vary from time to time. You agree
+          and accept these terms and conditions when using this website and the
+          services it provides. If you do not agree or accept these terms and
+          conditions, you should not continue to use healer.pk
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          1. Using the Site
+        </Typography>
+        <Typography>
+          healer.pk is for non-commercial personal use only. With any purchases
+          made on our site you agree to be legally bound by: Our terms and
+          conditions and any documents referred to within them; Any extra terms
+          which may be added or could replace some of this contract. This could
+          happen for regulatory or legal changes or for security reasons; and,
+          terms which specifically apply to certain products. You understand
+          that you are solely respoxible for: Any and all costs or expenses you
+          may incur when using our site; and Keeping your passwords and other
+          account information confidential We endeavor to keep the website as
+          accessible as possible to all who wish to use it. If you encounter any
+          issues or difficulties, please contact our customer service at
+          info@healer.pk If you do not comply to these terms and policies, we
+          have the right to suspend and restrict you your access to our site.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          2 Rights and Responsibilities
+        </Typography>
+        <Typography>
+          This Agreement highlights both yours and our legal rights and
+          responsibilities outlining key information necessary under the
+          Consumer Undertaking a consultation and purchasing treatment are
+          subject to the terms and conditions of this agreement. It is in your
+          best interest that we share information or contact your UP (Unani
+          Physician) about your consultation. However, without your consent we
+          cannot proceed to do so, therefore the prescriber will take full
+          responsibility for your care and after care.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          Warranties
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          Warranties by you
+        </Typography>
+        <Typography>
+          You agree when completing our online consultation questionnaire that
+          all disclosed information is truthful and honest and to the best of
+          your knowledge. Any questions answered untruthfully instantly voids
+          this agreement.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          Warranties by us
+        </Typography>
+        <Typography>
+          We agree and confirm that your consultation questionnaire will be
+          reviewed only by the prescriber and our dawakhana team for the purpose
+          of treating your condition. We agree and confirm that any medication
+          prescribed and supplied is dispensed by qualified physician
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          Description of medication
+        </Typography>
+        <Typography>
+          The description of medication is dependent on the prescription.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          3. Price
+        </Typography>
+        <Typography>
+          The price of the medication is dependent on the prescription. We may
+          need to contact you: dawakhana team or prescriber should we not accept
+          your order. For example, this may be due to the following reasons:
+          <Typography>Multiple treatment for the same indication</Typography>
+          <Typography>Referral to your UP</Typography>
+          <Typography>Product availability</Typography>
+          (Please note, not limited to the list above) Your order will be
+          considered as confirmed when you receive an email confirming dispatch.
+          At this point the prescriber has accepted your consultation. The
+          dawakhana has dispensed your prescription and your treatment has been
+          dispatched for delivery to you. This is a legal binding contract
+          between yourself and us and we will dispatch the medicines to you. If
+          the requested medication is unavailable for supply, a substitute or
+          alternative may need to be considered. In this instance, we will
+          contact you if we intend to do this. You have the right to refuse
+          supply of alternative treatments in which case a refund will be
+          offered. Packaging may vary to what is displayed on our site. In this
+          agreement your legal rights under relevant acts remain unaffected. We
+          endeavour to ensure that all payment information you provide us for
+          the payment of goods are secure with the use of an encrypted payment
+          gateway. However, in absence of negligence on our part we are not
+          legally responsible to you for any loss that you may suffer if a third
+          party gains unauthorized access to any information that you give us.
+          Your chosen payment card will only be charged once the checkout is
+          completed. All payments will forgo authorisation checks by the
+          relevant card issuer The price of medicines in PKR.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          4. Ownership and Intellectual Property Rights
+        </Typography>
+        <Typography>
+          We own this Site and all Intellectual Property Rights in it including
+          but not limited to any Content. Intellectual Property Rights mean
+          rights such as: copyright, trademarks, domain names, design rights,
+          database rights, patents and all other Intellectual Property Rights of
+          any kind whether or not they are registered or unregistered (anywhere
+          in the world). We reserve all of our rights in any intellectual
+          property in connection with these terms and conditions. This means,
+          for example, that we remain owners of them and free to use them as we
+          see fit.
+        </Typography>
+        <Typography>
+          All material contained and presented on this Site, including but not
+          limited to text and graphics is our copyright or similar rights,
+          unless otherwise expressly stated. We reserve all its rights and
+          strictly forbid any unauthorized use or duplication. Any statutorily
+          authorized rights to print or download are strictly limited for your
+          sole personal use. No material, however, can be used for any
+          commercial purpose whatsoever.
+        </Typography>
+        <Typography>
+          The use of any trade marks on the Site is strictly prohibited unless
+          you have our prior written permission. Any copies of the pages of this
+          Site which you save, by any means whatsoever can only be used for
+          subsequent viewing purposes or to print extracts for personal use.
+          Unless otherwise expressly permitted in writing, you may not create a
+          database in any form whatsoever of these webpages.
+        </Typography>
+        <Typography>
+          You agree not to adjust, to try to circumvent, or delete any notices
+          contained on the Site (including any intellectual property notices)
+          and in particular in any digital rights or other security technology
+          embedded or contained within the Site.
+        </Typography>
+        <Typography>
+          Nothing in these terms and conditions grants you any legal rights in
+          the Site other than as necessary to enable you to access the Site.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          5.Submitting information to the website
+        </Typography>
+        <Typography>
+          Any Unwanted Submissions may be used as we see reasonably fit on a
+          free-of-charge basis (this is on the basis that we are unaware and
+          have no way of knowing that the information is confidential,
+          commercially sensitive or valuable as we do not monitor the site to
+          check these matters). For this reason, we will not be legally
+          responsible for keeping any Unwanted Submissions confidential nor will
+          we be legally responsible to you or anybody else for any use of such
+          Unwanted Submissions. By using this website and providing us with your
+          information, you understand and indicate consent to us, our group and
+          third parties collecting and using your personal information in
+          accordance with the terms set out in this Agreement. We will only use
+          the information that we collect about you lawfully and in accordance
+          with the relevant Acts.
+        </Typography>
+        <Typography>
+          By using this website and providing us with your information, you
+          understand and indicate consent to us, our group and third parties
+          collecting and using your personal information in accordance with the
+          terms set out in this Agreement.
+        </Typography>
+        <Typography>
+          We will only use the information that we collect about you lawfully
+          and in accordance with the relevant Acts.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          6 Accuracy of information and availability of the site
+        </Typography>
+        <Typography>
+          We do not warrant the accuracy, reliability or continuous supply of
+          any information on this website. We try to make sure the information
+          on the website is accurate, free from bugs and up-to-date, but we
+          cannot promise that it will be. To add to this, we cannot guarantee
+          the website will be suitable for any purpose. You are at your own risk
+          if you rely on the information on this website.
+        </Typography>
+        <Typography>
+          Content is provided for general information purposes only and is to
+          inform you about us, our products and services, news, features and
+          other websites that may be of interest. It does not give technical,
+          financial or legal advice nor does it share any type of advice and
+          should not be relied on for any purposes.
+        </Typography>
+        <Typography>
+          We do not guarantee that you will be able to use the site
+          uninterrupted nor can we promise the site will be available at all
+          times.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          7 Hyperlinks and third-party sites
+        </Typography>
+        <Typography>
+          Where our website contains links to other websites and resources
+          provided by third parties, these links are provided for your
+          information only. We have no control over the contents of those
+          websites or resources, and accept no responsibility for them or for
+          any loss or damage that may arise from your use of them.
+        </Typography>
+        <Typography>
+          Where our website contains links to other websites and resources
+          provided by third parties, these links are provided for your
+          information only. We have no control over the contents of those
+          websites or resources, and accept no responsibility for them or for
+          any loss or damage that may arise from your use of them.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          8 Limitation on our liability
+        </Typography>
+        <Typography>
+          The material displayed on our website is provided without any
+          guarantees, conditions or warranties as to its accuracy. All
+          conditions, warranties and other terms, which might otherwise be
+          implied by statute, common law or the law of equity. Any liability for
+          any direct, indirect or consequential loss or damage incurred by any
+          user in connection with our website or in connection with the use,
+          inability to use, or results of the use of our website and Services,
+          any websites linked to it and any materials posted on it, including,
+          without limitation any liability for:
+        </Typography>
+        <Typography>
+          <Typography>loss of income or revenue</Typography>
+          <Typography>loss of business</Typography>
+          <Typography>loss of profits or contracts</Typography>
+          <Typography>loss of anticipated savings</Typography>
+          <Typography>loss of data</Typography>
+          <Typography>loss of goodwill;</Typography>
+        </Typography>
+        <Typography>
+          wasted management or office time; and for any other loss or damage of
+          any kind, however arising and whether caused by tort (including
+          negligence), breach of contract or otherwise, even if foreseeable,
+          provided that this condition shall not prevent claims for loss of or
+          damage to your tangible property or any other claims for direct
+          financial loss that are not excluded by any of the categories set out
+          above.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          9. Viruses, hacking and other offences
+        </Typography>
+        <Typography>
+          You must not misuse our website by knowingly introducing viruses,
+          trojans, worms, logic bombs or other material which is malicious or
+          technologically harmful.
+        </Typography>
+        <Typography>
+          You must not attempt to gain unauthorized access to our site, the
+          server on which our website is stored or any server, computer or
+          database connected to our site.
+        </Typography>
+        <Typography>
+          We will not be liable for any loss or damage caused by a distributed
+          denial-of-service attack, viruses or other technologically harmful
+          material that may infect your computer equipment, computer programs,
+          data or other proprietary material due to your use of our site or to
+          your downloading of any material posted on it, or on any website
+          linked to it.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          10 Events beyond our control
+        </Typography>
+        <Typography>
+          We will not be liable to you for any breach of these terms caused by
+          any event or circumstance beyond our reasonable control including,
+          without limitation, strikes, lock-outs or other industrial disputes,
+          breakdown of systems or network access, or flood, fire, explosion or
+          accident.
+        </Typography>
+        <Typography className={styles.heading} variant="h5">
+          11 Variation
+        </Typography>
+        <Typography>
+          We may revise these terms of use at any time by amending this page.
+          You are expected to check this page from time to time to take notice
+          of any changes we made, as they are binding on you. Our new terms will
+          be displayed on the Site and by continuing to use and access the Site
+          following such changes, you agree to be bound by any variation made by
+          us. Some of the provisions contained in these terms and conditions may
+          also be superseded by provisions or notices published elsewhere on our
+          website.
+        </Typography>
+        <Typography>
+          We try to resolve any disputes raised by you efficiently and
+          accurately through our internal complaint&apos;s procedure.
+        </Typography>
+        <Typography>
+          If you are unhappy with any of the following please contact us
+          immediately following the complaint&apos;s procedure:
+        </Typography>
+        <Typography>
+          The service you have received, the medicines supplied or any other
+          matter.
+        </Typography>
+      </div>
     </Layout>
   );
 }
