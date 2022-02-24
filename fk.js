132a2b17866791b286045df86f71ba7087599315
Muhammad Rahman <mjc8808@gmail.com>
2022-02-25 01:59:40 +0500

    top brands added

---
+++ fk.js
@@ -0,0 +1,48 @@
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper";
+
+{
+  /* <Swiper
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 3000,
+        }}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper">
+        <SwiperSlide>
+          <Image
+            src="/homeImg1.jpg"
+            className={styles.carousel_img}
+            alt="homeImg1"
+            layout="responsive"
+            width={400}
+            height={100}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/homeImg2.jpg"
+            className={styles.carousel_img}
+            alt="homeImg1"
+            layout="responsive"
+            width={400}
+            height={100}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/homeImg3.jpg"
+            className={styles.carousel_img}
+            alt="homeImg1"
+            layout="responsive"
+            width={400}
+            height={100}
+          />
+        </SwiperSlide>
+      </Swiper> */
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "@/components/Layout";
 import styles from "@/styles/Home.module.css";
 import Image from "next/image";
 
-import "swiper/css";
-import "swiper/css/pagination";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination } from "swiper";
+import { Avatar, Typography, Divider } from "@mui/material";
 
 export default function Home() {
+  const brands = [
+    { img: "/brandsIcons/Ajmal.png", name: "Ajmal" },
+    { img: "/brandsIcons/Aftab Qarshi.png", name: "Aftab Qarshi" },
+    { img: "/brandsIcons/Ashraf.png", name: "Ashraf" },
+    { img: "/brandsIcons/Brick.jpeg", name: "Brick" },
+    { img: "/brandsIcons/Hamdard.jpeg", name: "Hamdard" },
+    { img: "/brandsIcons/Marhaba.png", name: "Marhaba" },
+    { img: "/brandsIcons/Qarshi.png", name: "Qarshi" },
+    { img: "/brandsIcons/Tayyebi.png", name: "Tayyebi" },
+    { img: "/brandsIcons/TT.jpeg", name: "TT" },
+    { img: "/brandsIcons/Uppal.jpeg", name: "Uppal" },
+  ];
+
+  const [shuffledArray, setShuffledArray] = useState([]);
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+    setShuffledArray(array);
+  }
+
+  // console.log(shuffledArray);
+
+  useEffect(() => {
+    shuffle(brands);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Layout>
-      {/* <Swiper
-        pagination={{
-          clickable: true,
-        }}
-        autoplay={{
-          delay: 3000,
-        }}
-        modules={[Pagination, Autoplay]}
-        className="mySwiper">
-        <SwiperSlide>
-          <Image
-            src="/homeImg1.jpg"
-            className={styles.carousel_img}
-            alt="homeImg1"
-            layout="responsive"
-            width={400}
-            height={100}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="/homeImg2.jpg"
-            className={styles.carousel_img}
-            alt="homeImg1"
-            layout="responsive"
-            width={400}
-            height={100}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="/homeImg3.jpg"
-            className={styles.carousel_img}
-            alt="homeImg1"
-            layout="responsive"
-            width={400}
-            height={100}
-          />
-        </SwiperSlide>
-      </Swiper> */}
-
       <div className={styles.bannerContainer}>
         <Image
+          priority
           className={styles.image}
           alt="banner"
           src="/banners.jpeg"
           layout="fill"
         />
+      </div>
+      <Typography sx={{ margin: "20px 0 0 10px" }} variant="h5">
+        Top Brands
+      </Typography>
+      <div className={styles.topBrands}>
+        {shuffledArray.map((brand) => (
+          <div className={styles.brandCard} key={brand.name}>
+            <div className={styles.avatar}>
+              <div className={styles.avatar__img}>
+                <Image
+                  src={brand.img}
+                  alt={brand.name}
+                  width="65px"
+                  height="45px"
+                />
+              </div>
+            </div>
+            <Typography sx={{ opacity: "0.8" }}>{brand.name}</Typography>
+          </div>
+        ))}
       </div>
     </Layout>
   );
