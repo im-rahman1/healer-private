e622cd92285b419ac94bdc35863de65c9fda0e19
Muhammad Rahman <mjc8808@gmail.com>
2022-03-26 12:15:03 +0500

    added cart

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,5 +1,10 @@
-export default  function Cart() {
-    return (
-        <div>Cart</div>
-    )
+import Layout from "@/components/Layout";
+import styles from "@/styles/cart.module.css";
+
+export default function Cart() {
+  return (
+    <Layout title="Cart">
+      <div>Cart</div>
+    </Layout>
+  );
 }
