187bddc799f4dd07673a47a8cf3d9f24076402bf
Muhammad Rahman <mjc8808@gmail.com>
2022-03-29 11:46:51 +0500

    add analytics

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import "@/styles/globals.css";
+import Script from "next/script";
+import { GA_MEASUREMENT_ID } from "@/config/config";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', "${GA_MEASUREMENT_ID}", {
+            page_path: window.location.pathname
+          });
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
