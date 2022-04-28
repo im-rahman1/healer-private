4002ba4b5d72be3066e1b1273bed51431c57e15a
im-rahman1 <mjc8808@gmail.com>
2022-04-28 15:17:13 +0500

    done with docCards

---
+++ pages/physicians/index.js
@@ -0,0 +1,161 @@
+import React from "react";
+import styles from "@/styles/physicians.module.css";
+import Layout from "@/components/Layout";
+import Link from "next/link";
+
+// mui
+import { Avatar, Breadcrumbs, Typography, Rating } from "@mui/material";
+import DoneIcon from "@mui/icons-material/Done";
+import CircleIcon from '@mui/icons-material/Circle';
+
+const muiStyles = {
+  link: {
+    cursor: "pointer",
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+  avatar: {
+    width: "5rem",
+    height: "5rem",
+  },
+};
+
+export default function Physicians() {
+  const breadcrumbs = [
+    <Link href="/" key="1" color="inherit" passHref>
+      <Typography sx={muiStyles.link}>Home</Typography>
+    </Link>,
+    <Typography href="/" key="3" color="text.primary">
+      Physicians
+    </Typography>,
+  ];
+
+  return (
+    <Layout title="Physicians">
+      <Breadcrumbs separator="›" aria-label="breadcrumb">
+        {breadcrumbs}
+      </Breadcrumbs>
+      <div className={styles.docCards}>
+        <div className={styles.docCard}>
+          <div className={styles.bar}></div>
+          <div className={styles.profileInfoContainer}>
+            <div className={styles.profileInfo}>
+              <Avatar src="" sx={muiStyles.avatar} />
+              <div className={styles.profileInfo__text}>
+                <Typography>Dr. Muhammad Rahman</Typography>
+                <Typography variant="caption" sx={{color: '#01a22e'}}>Unani Physician</Typography>
+                <Typography variant="caption">MBBS, phd</Typography>
+                <Typography variant="caption">12 years of experience</Typography>
+              </div>
+            </div>
+            <div className={styles.flexEnd}>
+              <div className={styles.reviewContainer}>
+                <div className={styles.badgeContainer}>
+                  <DoneIcon sx={{ color: "#01a22e" }} />
+                  <div className={styles.badgeTxt}>
+                    <Typography sx={{whiteSpace: 'noWrap'}} variant="body2">NCT Verified</Typography>
+                  </div>
+                </div>
+                <div className={styles.rating}>
+                  <Rating value={3.5} precision={0.5} max={4} sx={{color: '#01a22e'}} size="small" readOnly />
+                  <Typography variant="body2">3.5</Typography>
+                </div>
+                <Typography variant="body2">30 reviews</Typography>
+              </div>
+            </div>
+          </div>
+            <Typography sx={{marginLeft: '0.7rem'}}>Book Appointment</Typography>
+          <div className={styles.bookAppointmentContainer}>
+            <div className={styles.appointmentCard}>
+              <div>
+                <Typography>Online</Typography>
+                <Typography>Video Consultation</Typography>
+                <div className={styles.status}>
+                <CircleIcon sx={{fontSize: '0.7rem', opacity: 0.8, color: '#01a22e'}} />
+                <Typography variant='caption'>Available Today</Typography>
+                </div>
+              </div>
+              <div className={styles.pricing}>
+                <Typography>Rs 1000</Typography>
+              </div>
+            </div>
+            <div className={styles.appointmentCard}>
+              <div className={styles.appointment__info}>
+                <Typography>InClinic</Typography>
+                <Typography variant='caption'>Al Noor clinic</Typography>
+                <Typography variant='caption'>Main crossing joharTown</Typography>
+                <div className={styles.status}>
+                <CircleIcon sx={{fontSize: '0.7rem', opacity: 0.8, color: '#01a22e'}} />
+                <Typography variant='caption'>Available Today</Typography>
+                </div>
+              </div>
+              <div className={styles.pricing}>
+                <Typography>RS 1000</Typography>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.docCard}>
+          <div className={styles.bar}></div>
+          <div className={styles.profileInfoContainer}>
+            <div className={styles.profileInfo}>
+              <Avatar src="" sx={muiStyles.avatar} />
+              <div className={styles.profileInfo__text}>
+                <Typography>Dr. Muhammad Rahman</Typography>
+                <Typography variant="caption" sx={{color: '#01a22e'}}>Unani Physician</Typography>
+                <Typography variant="caption">MBBS, phd</Typography>
+                <Typography variant="caption">12 years of experience</Typography>
+              </div>
+            </div>
+            <div className={styles.flexEnd}>
+              <div className={styles.reviewContainer}>
+                <div className={styles.badgeContainer}>
+                  <DoneIcon sx={{ color: "#01a22e" }} />
+                  <div className={styles.badgeTxt}>
+                    <Typography sx={{whiteSpace: 'noWrap'}} variant="body2">NCT Verified</Typography>
+                  </div>
+                </div>
+                <div className={styles.rating}>
+                  <Rating value={3.5} precision={0.5} max={4} sx={{color: '#01a22e'}} size="small" readOnly />
+                  <Typography variant="body2">3.5</Typography>
+                </div>
+                <Typography variant="body2">30 reviews</Typography>
+              </div>
+            </div>
+          </div>
+            <Typography sx={{marginLeft: '0.7rem'}}>Book Appointment</Typography>
+          <div className={styles.bookAppointmentContainer}>
+            <div className={styles.appointmentCard}>
+              <div>
+                <Typography>Online</Typography>
+                <Typography>Video Consultation</Typography>
+                <div className={styles.status}>
+                <CircleIcon sx={{fontSize: '0.7rem', opacity: 0.8, color: '#01a22e'}} />
+                <Typography variant='caption'>Available Today</Typography>
+                </div>
+              </div>
+              <div className={styles.pricing}>
+                <Typography>Rs 1000</Typography>
+              </div>
+            </div>
+            <div className={styles.appointmentCard}>
+              <div className={styles.appointment__info}>
+                <Typography>InClinic</Typography>
+                <Typography variant='caption'>Al Noor clinic</Typography>
+                <Typography variant='caption'>Main crossing joharTown</Typography>
+                <div className={styles.status}>
+                <CircleIcon sx={{fontSize: '0.7rem', opacity: 0.8, color: '#01a22e'}} />
+                <Typography variant='caption'>Available Today</Typography>
+                </div>
+              </div>
+              <div className={styles.pricing}>
+                <Typography>RS 1000</Typography>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
