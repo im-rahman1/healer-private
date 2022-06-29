f18901074d8e7dc3be87f557c996fb9df07d9d68
Muhammad Rahman <63861321+im-rahman1@users.noreply.github.com>
2022-06-29 16:44:08 +0500

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDunpGuxdx7VJ7RB2lt8iAASkuTbZYCqIY",
-  authDomain: "healercare-b6b7f.firebaseapp.com",
-  projectId: "healercare-b6b7f",
-  storageBucket: "healercare-b6b7f.appspot.com",
-  messagingSenderId: "884106203484",
-  appId: "1:884106203484:web:64a069266b6607871a3199",
-  measurementId: "G-6XHGRH9T28"
+//   apiKey: "AIzaSyDunpGuxdx7VJ7RB2lt8iAASkuTbZYCqIY",
+//   authDomain: "healercare-b6b7f.firebaseapp.com",
+//   projectId: "healercare-b6b7f",
+//   storageBucket: "healercare-b6b7f.appspot.com",
+//   messagingSenderId: "884106203484",
+//   appId: "1:884106203484:web:64a069266b6607871a3199",
+//   measurementId: "G-6XHGRH9T28"
 };
 
 const app = initializeApp(firebaseConfig);
